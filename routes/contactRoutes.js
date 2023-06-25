bbf9a8b26864ac87c7fbc3e296e8e7ab2c0562c4
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2023-06-25 17:45:58 +0530

    Protecting Routes-Contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
-const{getContacts,getContact,createContact,updateContact,deleteContact}=require("../controllers/contactController")
+const{getContacts,getContact,createContact,updateContact,deleteContact}=require("../controllers/contactController");
+const { validateToken } = require('../middleware/validateTokenHandler');
 // router.route("/").get((req,res)=>{
 //   res.status(201).json({message:"get all the contacts"})  
 // })
+router.use(validateToken);
 router.route("/").get(getContacts).post(createContact)
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact)
 module.exports=router;
